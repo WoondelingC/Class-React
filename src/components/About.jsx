795d0b1468f711d893a6124ab72d1ea9238a6a36
Luuchoh <luxo997@gmail.com>
2021-07-28 16:28:00 -0500

    Fusionando componente about con el contenedor portafolio

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,15 +5,16 @@ import styled from 'styled-components';
 
 const AboutStyle = styled.div`
     text-align: center;
-    width: 250px;
-    margin:100px 0 0 300px;
-    border: 2px solid blue;
+    width: 100%;
+    height: 100vh;
     border-radius: 20px;
     padding:10px;
 `;
 
 
 const AboutAvatar = styled.div`
+    margin-top: 30px;
+    margin-bottom: 20%;
     padding:0;
 `;
 
@@ -29,17 +30,19 @@ const AboutImg = styled.img`
 
 const AboutName = styled.div`
     text-align: center;
+
 `;
 
 const AboutInf = styled.div `
-text-align: center;
-margin-bottom:50px;
+    text-align: center;
+    padding: 10px 0;
+    margin-bottom:40px;
 `;
 const AboutFooter = styled.img `
-width: 20px;
-height: 20px;
-display: flex-inline;
-margin-left: 20px;
+    width: 20px;
+    height: 20px;
+    display: flex-inline;
+    margin: 20px;
 `
 
 //funcion statefull
@@ -52,11 +55,12 @@ export const About = () => {
                   <AboutAvatar>
                     <figure>
                       <AboutImg src="https://3.bp.blogspot.com/-JfL1o7oSnKI/VmodObHF9cI/AAAAAAAABLY/nKKRXw0-yiU/s1600/homero_456_336.jpg" alt="Mi foto" />
-                    </figure>
-                    <AboutName>
+                    </figure>  
+                  </AboutAvatar>
+
+                  <AboutName>
                       <h2>Woondeling Jose Colina Martinez</h2>
                     </AboutName>
-                  </AboutAvatar>
               </div>
               <AboutInf>
               <p>Estudiante del curso oficial Frontend</p>
