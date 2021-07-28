c87abd3ccb1f586b80231e1226cbc7ec733a13d5
Luuchoh <luxo997@gmail.com>
2021-07-28 17:23:57 -0500

    estilizando el scroll

--- src/components/EstudiosExperiencia.jsx
+++ src/components/EstudiosExperiencia.jsx
@@ -10,8 +10,24 @@ const Container = styled.div`
     justify-content: space-evenly;
     align-items: flex-start;
     padding: 20px 30px;
-    overflow: scroll;
+    overflow-x: hidden;
+    overflow-y: scroll;
+    scrollbar-width: thin;
     box-sizing: border-box;
+
+    &::-webkit-scrollbar {
+        width: 7px;
+        height: 90%;
+        background-color: inherit;
+    }
+    &::-webkit-scrollbar-track{
+        border-radius: 10px;
+        background-color: inherit;
+    }
+    &::-webkit-scrollbar-thumb{
+        background-color: #797979;
+        border-radius: 20px;
+}
 `;
 
 const Studies = styled.div`
