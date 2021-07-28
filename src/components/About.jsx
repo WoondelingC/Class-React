68600d6c2afb0a55a8db8a4dba0014f8b98a0855
Woondeling Colina <josecolinamartinez@gmail.com>
2021-07-28 16:12:51 -0500

    se agrego imagen y informacion al perfil

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,10 +5,16 @@ import styled from 'styled-components';
 
 const AboutStyle = styled.div`
     text-align: center;
+    width: 250px;
+    margin:100px 0 0 300px;
+    border: 2px solid blue;
+    border-radius: 20px;
+    padding:10px;
 `;
 
+
 const AboutAvatar = styled.div`
-    padding: 2em 0 0 0;
+    padding:0;
 `;
 
 const AboutImg = styled.img`
@@ -25,6 +31,17 @@ const AboutName = styled.div`
     text-align: center;
 `;
 
+const AboutInf = styled.div `
+text-align: center;
+margin-bottom:50px;
+`;
+const AboutFooter = styled.img `
+width: 20px;
+height: 20px;
+display: flex-inline;
+margin-left: 20px;
+`
+
 //funcion statefull
 export const About = () => {
     return (
@@ -37,13 +54,26 @@ export const About = () => {
                       <AboutImg src="https://3.bp.blogspot.com/-JfL1o7oSnKI/VmodObHF9cI/AAAAAAAABLY/nKKRXw0-yiU/s1600/homero_456_336.jpg" alt="Mi foto" />
                     </figure>
                     <AboutName>
-                      <h2>Woondeling</h2>
+                      <h2>Woondeling Jose Colina Martinez</h2>
                     </AboutName>
                   </AboutAvatar>
               </div>
+              <AboutInf>
+              <p>Estudiante del curso oficial Frontend</p>
+              </AboutInf>
+              <div>
+                  Bogotá, Colombia 
+              </div>
+              <footer>
+                  <figure>
+                      <AboutFooter src="https://images.vexels.com/media/users/3/137253/isolated/lists/90dd9f12fdd1eefb8c8976903944c026-logotipo-del-icono-de-facebook.png" alt="" />
+                      <AboutFooter src="https://p.kindpng.com/picc/s/378-3783009_icono-pequeo-de-linkedin-hd-png-download.png" alt="" />
+                      <AboutFooter src="https://image.flaticon.com/icons/png/512/25/25231.png" alt="" />
+                  </figure>
+              </footer>
               </AboutStyle> 
-          </div>  
-        
+          </div>
+          
     )
 }
 
