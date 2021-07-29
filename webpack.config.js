17fa982fcf7d824f049289787dfc60ddf7e7c384
Woondeling Colina <josecolinamartinez@gmail.com>
2021-07-28 22:58:39 -0500

    actualizando el puerto

--- webpack.config.js
+++ webpack.config.js
@@ -73,5 +73,6 @@ module.exports = {
     compress: true,
     //Habilitar para poder acceder a la información y mostrar cada una de las rutas
     historyApiFallback: true,
+    port: 3006,
   },
 };
