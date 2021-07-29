4073ece2931a8b03d160d889d44ba3c284da21ac
Woondeling Colina <josecolinamartinez@gmail.com>
2021-07-28 20:13:01 -0500

    se corrigio error de escritura

--- src/components/Certificaciones.jsx
+++ src/components/Certificaciones.jsx
@@ -16,7 +16,7 @@ export default class Certificaciones extends Component {
                 <h1>Certificaciones</h1>
                 <p>Ingenieria de Petroleo</p>
                 <p>Ingeniero de Yacimientos</p>
-                <p>Monitoreo , acualizacion de base de datos local de pozos</p>
+                <p>Monitoreo y actualizacion de base de datos local de pozos</p>
                 <p>Curso de Javascript Platzi</p>
             </Certificacion>
         )
